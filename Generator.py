c1b8bbc0c136bf20031b88c1333468253706859a
Giacomo <giac290595@gmail.com>
2018-08-28 12:42:29 +0200

    ended script

---
+++ Generator.py
@@ -0,0 +1,11 @@
+menu = "Insert extension of the file:\n "
+
+def main():
+    ext = input(menu)
+    nome = ""
+    while nome != "end":
+        nome = input("FILENAME:\n\n")
+        file = open ("%s.%s" % (nome, ext), "w+")
+        file.close()
+
+main()
